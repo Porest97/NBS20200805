06253f3d805649b033f84bd053e5e0347e7821f5
Per Orest <per.orest@outlook.com>
2020-08-06 16:38:49 +0200

    Added all functionalities for Incidents

--- Controllers/ApplicationControllers/IncidentsController.cs
+++ Controllers/ApplicationControllers/IncidentsController.cs
@@ -22,10 +22,185 @@ namespace NBS.Controllers.ApplicationControllers
         // GET: Incidents
         public async Task<IActionResult> Index()
         {
-            var nBSContext = _context.Incident.Include(i => i.Creator).Include(i => i.FEAssigned).Include(i => i.IncidentPriority).Include(i => i.IncidentStatus).Include(i => i.IncidentType).Include(i => i.MtrlList).Include(i => i.PurchaseOrder).Include(i => i.Receiver).Include(i => i.Site);
+            var nBSContext = _context.Incident
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.MtrlList)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site);
             return View(await nBSContext.ToListAsync());
         }
 
+        // GET: Incidents - search
+        public async Task<IActionResult> IndexSearch
+            (string searchString, string searchString1,
+            string searchString2, string searchString3,
+            string searchString4, string searchString5,
+            string searchString6, string searchString7)
+        {
+            var incidents = from i in _context.Incident
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+
+                            select i;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.IncidentNumber.Contains(searchString));
+
+            }
+            if (!String.IsNullOrEmpty(searchString1))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.Site.SiteNumber.Contains(searchString1));
+            }
+            if (!String.IsNullOrEmpty(searchString2))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.Description.Contains(searchString2));
+            }
+            if (!String.IsNullOrEmpty(searchString3))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.IncidentStatus.IncidentStatusName.Contains(searchString3));
+            }
+            if (!String.IsNullOrEmpty(searchString4))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.IncidentPriority.IncidentPriorityName.Contains(searchString4));
+
+            }
+            if (!String.IsNullOrEmpty(searchString5))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.IncidentType.IncidentTypeName.Contains(searchString5));
+            }
+            if (!String.IsNullOrEmpty(searchString6))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.PurchaseOrder.PONumber.Contains(searchString6));
+            }
+            if (!String.IsNullOrEmpty(searchString7))
+            {
+                incidents = incidents
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.MtrlList)
+                .Where(s => s.Creator.FirstName.Contains(searchString7));
+            }
+
+            return View(await incidents.ToListAsync());
+        }
+
+        // GET: Incidents/DetailsPrint/5
+        public async Task<IActionResult> DetailsPrint(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var incident = await _context.Incident
+                .Include(i => i.Creator)
+                .Include(i => i.FEAssigned)
+                .Include(i => i.IncidentPriority)
+                .Include(i => i.IncidentStatus)
+                .Include(i => i.IncidentType)
+                .Include(i => i.PurchaseOrder)
+                .Include(i => i.Receiver)
+                .Include(i => i.Site)
+                .Include(i => i.Site).Include(i => i.MtrlList)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            return View(incident);
+        }
+
+        
         // GET: Incidents/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -56,15 +231,15 @@ namespace NBS.Controllers.ApplicationControllers
         // GET: Incidents/Create
         public IActionResult Create()
         {
-            ViewData["PersonId"] = new SelectList(_context.Person, "Id", "Id");
-            ViewData["PersonId2"] = new SelectList(_context.Person, "Id", "Id");
-            ViewData["IncidentPriorityId"] = new SelectList(_context.IncidentPriority, "Id", "Id");
-            ViewData["IncidentStatusId"] = new SelectList(_context.IncidentStatus, "Id", "Id");
-            ViewData["IncidentTypeId"] = new SelectList(_context.IncidentType, "Id", "Id");
-            ViewData["MtrlListId"] = new SelectList(_context.MtrlList, "Id", "Id");
-            ViewData["PurchaseOrderId"] = new SelectList(_context.PurchaseOrder, "Id", "Id");
-            ViewData["PersonId1"] = new SelectList(_context.Person, "Id", "Id");
-            ViewData["SiteId"] = new SelectList(_context.Site, "Id", "Id");
+            ViewData["MtrlListId"] = new SelectList(_context.Set<MtrlList>(), "Id", "Description");
+            ViewData["PersonId"] = new SelectList(_context.Set<Person>(), "Id", "FullName");
+            ViewData["PersonId2"] = new SelectList(_context.Set<Person>(), "Id", "FullName");
+            ViewData["IncidentPriorityId"] = new SelectList(_context.Set<IncidentPriority>(), "Id", "IncidentPriorityName");
+            ViewData["IncidentStatusId"] = new SelectList(_context.Set<IncidentStatus>(), "Id", "IncidentStatusName");
+            ViewData["IncidentTypeId"] = new SelectList(_context.Set<IncidentType>(), "Id", "IncidentTypeName");
+            ViewData["PurchaseOrderId"] = new SelectList(_context.Set<PurchaseOrder>(), "Id", "PONumber");
+            ViewData["PersonId1"] = new SelectList(_context.Set<Person>(), "Id", "FullName");
+            ViewData["SiteId"] = new SelectList(_context.Set<Site>(), "Id", "NoSite");
             return View();
         }
 
@@ -81,15 +256,15 @@ namespace NBS.Controllers.ApplicationControllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["PersonId"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId);
-            ViewData["PersonId2"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId2);
-            ViewData["IncidentPriorityId"] = new SelectList(_context.IncidentPriority, "Id", "Id", incident.IncidentPriorityId);
-            ViewData["IncidentStatusId"] = new SelectList(_context.IncidentStatus, "Id", "Id", incident.IncidentStatusId);
-            ViewData["IncidentTypeId"] = new SelectList(_context.IncidentType, "Id", "Id", incident.IncidentTypeId);
-            ViewData["MtrlListId"] = new SelectList(_context.MtrlList, "Id", "Id", incident.MtrlListId);
-            ViewData["PurchaseOrderId"] = new SelectList(_context.PurchaseOrder, "Id", "Id", incident.PurchaseOrderId);
-            ViewData["PersonId1"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId1);
-            ViewData["SiteId"] = new SelectList(_context.Site, "Id", "Id", incident.SiteId);
+            ViewData["MtrlListId"] = new SelectList(_context.Set<MtrlList>(), "Id", "Description", incident.MtrlListId);
+            ViewData["PersonId"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId);
+            ViewData["PersonId2"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId2);
+            ViewData["IncidentPriorityId"] = new SelectList(_context.Set<IncidentPriority>(), "Id", "IncidentPriorityName", incident.IncidentPriorityId);
+            ViewData["IncidentStatusId"] = new SelectList(_context.Set<IncidentStatus>(), "Id", "IncidentStatusName", incident.IncidentStatusId);
+            ViewData["IncidentTypeId"] = new SelectList(_context.Set<IncidentType>(), "Id", "IncidentTypeName", incident.IncidentTypeId);
+            ViewData["PurchaseOrderId"] = new SelectList(_context.Set<PurchaseOrder>(), "Id", "PONumber", incident.PurchaseOrderId);
+            ViewData["PersonId1"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId1);
+            ViewData["SiteId"] = new SelectList(_context.Set<Site>(), "Id", "NoSite", incident.SiteId);
             return View(incident);
         }
 
@@ -106,15 +281,15 @@ namespace NBS.Controllers.ApplicationControllers
             {
                 return NotFound();
             }
-            ViewData["PersonId"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId);
-            ViewData["PersonId2"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId2);
-            ViewData["IncidentPriorityId"] = new SelectList(_context.IncidentPriority, "Id", "Id", incident.IncidentPriorityId);
-            ViewData["IncidentStatusId"] = new SelectList(_context.IncidentStatus, "Id", "Id", incident.IncidentStatusId);
-            ViewData["IncidentTypeId"] = new SelectList(_context.IncidentType, "Id", "Id", incident.IncidentTypeId);
-            ViewData["MtrlListId"] = new SelectList(_context.MtrlList, "Id", "Id", incident.MtrlListId);
-            ViewData["PurchaseOrderId"] = new SelectList(_context.PurchaseOrder, "Id", "Id", incident.PurchaseOrderId);
-            ViewData["PersonId1"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId1);
-            ViewData["SiteId"] = new SelectList(_context.Site, "Id", "Id", incident.SiteId);
+            ViewData["MtrlListId"] = new SelectList(_context.Set<MtrlList>(), "Id", "Description", incident.MtrlListId);
+            ViewData["PersonId"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId);
+            ViewData["PersonId2"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId2);
+            ViewData["IncidentPriorityId"] = new SelectList(_context.Set<IncidentPriority>(), "Id", "IncidentPriorityName", incident.IncidentPriorityId);
+            ViewData["IncidentStatusId"] = new SelectList(_context.Set<IncidentStatus>(), "Id", "IncidentStatusName", incident.IncidentStatusId);
+            ViewData["IncidentTypeId"] = new SelectList(_context.Set<IncidentType>(), "Id", "IncidentTypeName", incident.IncidentTypeId);
+            ViewData["PurchaseOrderId"] = new SelectList(_context.Set<PurchaseOrder>(), "Id", "PONumber", incident.PurchaseOrderId);
+            ViewData["PersonId1"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId1);
+            ViewData["SiteId"] = new SelectList(_context.Set<Site>(), "Id", "NoSite", incident.SiteId);
             return View(incident);
         }
 
@@ -150,15 +325,15 @@ namespace NBS.Controllers.ApplicationControllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["PersonId"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId);
-            ViewData["PersonId2"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId2);
-            ViewData["IncidentPriorityId"] = new SelectList(_context.IncidentPriority, "Id", "Id", incident.IncidentPriorityId);
-            ViewData["IncidentStatusId"] = new SelectList(_context.IncidentStatus, "Id", "Id", incident.IncidentStatusId);
-            ViewData["IncidentTypeId"] = new SelectList(_context.IncidentType, "Id", "Id", incident.IncidentTypeId);
-            ViewData["MtrlListId"] = new SelectList(_context.MtrlList, "Id", "Id", incident.MtrlListId);
-            ViewData["PurchaseOrderId"] = new SelectList(_context.PurchaseOrder, "Id", "Id", incident.PurchaseOrderId);
-            ViewData["PersonId1"] = new SelectList(_context.Person, "Id", "Id", incident.PersonId1);
-            ViewData["SiteId"] = new SelectList(_context.Site, "Id", "Id", incident.SiteId);
+            ViewData["MtrlListId"] = new SelectList(_context.Set<MtrlList>(), "Id", "Description", incident.MtrlListId);
+            ViewData["PersonId"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId);
+            ViewData["PersonId2"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId2);
+            ViewData["IncidentPriorityId"] = new SelectList(_context.Set<IncidentPriority>(), "Id", "IncidentPriorityName", incident.IncidentPriorityId);
+            ViewData["IncidentStatusId"] = new SelectList(_context.Set<IncidentStatus>(), "Id", "IncidentStatusName", incident.IncidentStatusId);
+            ViewData["IncidentTypeId"] = new SelectList(_context.Set<IncidentType>(), "Id", "IncidentTypeName", incident.IncidentTypeId);
+            ViewData["PurchaseOrderId"] = new SelectList(_context.Set<PurchaseOrder>(), "Id", "PONumber", incident.PurchaseOrderId);
+            ViewData["PersonId1"] = new SelectList(_context.Set<Person>(), "Id", "FullName", incident.PersonId1);
+            ViewData["SiteId"] = new SelectList(_context.Set<Site>(), "Id", "NoSite", incident.SiteId);
             return View(incident);
         }
 
